9f8ff7a3b4eb7365e697c1614e806b3472f36194
kalium <martijn@becker.wtf>
2017-06-12 23:08:24 +0200

    I made things.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const fs = require('fs')
 
-const file = process.argv[4] || './'
+const file = process.argv[4] || './temp'
 
 const name = process.argv[2] || 'statorium'
 
 const description = process.argv[3] || 'simplicity itself'
 
-fs.write(file)
+fs.readdir('./template/', (err, data) => {
+	if(!err) {
+	fs.mkdir(file, err =>
+	data.forEach(x =>  {
+		fs.readFile(`./template/${x}`, 'utf-8', (err, data) => {
+			if(!err)
+			fs.writeFile(`${file}/${x}`, data.replace('{NAME}', name).replace('{DESCRIPTION}', description), err => console.log('generated:', x))
+	})}))
+}})
